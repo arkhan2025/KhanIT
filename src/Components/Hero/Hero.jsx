044b2322d0fd5fce02ebe96edf920d47e0b7bfa5
Md Ashfaqur Rahman Khan <ashfaqur.rahman.khan.zayed@gmail.com>
2026-01-12 21:51:31 +0600

    Fix images and update 1 file

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Hero.css";
 import logo from '../../assets/Logo.png';
 import arrow from '../../assets/arrow.alt.left_24px.png';
-import call from '../../assets/call.png';
+import call from '../../assets/Call.png';
 
 const Hero = () => {
   const [menuOpen, setMenuOpen] = useState(false);
